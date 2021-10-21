976154af8a1097d81a30695dfed345bd117fa60a
Pooja Kushwah <pooja.kushwah@BLR.local>
2021-10-21 15:51:36 +0530

    align folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import './animation.css';
-import Todo from './components/index'
+import Todo from './components/reactAnimation/index'
 
 function App() {
   return (
